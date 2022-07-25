54d9e7b58ba34a7f2ea98f01af0654834c71bedb
lynnefa23-meet <st.lynne.al-faqih@rosaryshs-j.com>
2022-07-25 13:51:22 +0300

    done!

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-function ChangeBackgroundColor(color){
+blabla= false 
+function ChangeBackgroundColor(color, color2){
 	document.body.style.background= color;
+	if(blabla== false){
+		document.body.style.background= color;
+		blabla = true
+
+	}
+	else if (blabla== true){
+		document.body.style.background= color2;
+		blabla = false
+	}
+
 }
